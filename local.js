75d44cdf837694c558a33eb417cf2da5063e5b38
Ivaylo Novakov <inovakov@gmail.com>
2017-08-25 11:49:08 +0200

    Add some debug info. Add a breakpoint (`debugger`).

--- local.js
+++ local.js
@@ -1,9 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const debug = require('debug');
+const app = express();
+
+/* In order to debug:
+     - start the app as `node debug local.js`
+     - hit `c` to allow it to run freely (it will stop initially at the first line)
+     - the execution will pause on the first line with `debugger` on it
+*/
 
 app.get('/', (req, res) => {
+    debugger;
     res.send({
         status: 'hello world!'
     });
+    debug('info')('request served! (info)');
 });
 
 app.listen(3000, () => {
