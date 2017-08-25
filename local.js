97550df46349cb8a8a2267a347334f949ea9cfe8
Ivaylo Novakov <inovakov@gmail.com>
2017-08-25 12:07:50 +0200

    There is an actual debugger! With a UI and everything!

--- local.js
+++ local.js
@@ -6,6 +6,10 @@ const app = express();
      - start the app as `node debug local.js`
      - hit `c` to allow it to run freely (it will stop initially at the first line)
      - the execution will pause on the first line with `debugger` on it
+
+    Alternatively:
+     - run `node --inspect local.js`
+     - visit chrome://inspect or about:inspect (in Chrome)
 */
 
 app.get('/', (req, res) => {
