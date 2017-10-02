9705aa06117cafb35f26dfc13e3ab3e51032afe0
Ivaylo Novakov <inovakov@gmail.com>
2017-10-02 14:36:51 +0200

    Add a minimal AWS Lambda.

---
+++ lambda/app.js
@@ -0,0 +1,17 @@
+// const app = require('express')();
+//
+// app.get('/', sayHello);
+//
+// function sayHello(req, res) {
+//   res.end('Hello!');
+// }
+
+/*
+This function is all you need in order to power your AWS Lambda.
+Just comment everything else out, zip this file on its own and upload it to Lambda.
+Set you Handler to `app.handler` and it will run the code below.
+(If you need libraries you can create a package.json in this dir and zip them together.)
+*/
+exports.handler = (event, context, callback) => {
+
+};
