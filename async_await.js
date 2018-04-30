21a0a7574da7a7552b11c666e361397917359a1d
Ivaylo Novakov <inovakov@gmail.com>
2018-04-30 15:38:09 +0200

    More async/await.

--- async_await.js
+++ async_await.js
@@ -1,27 +1,23 @@
 'use strict';
 
-function getJSON() {
-  return Promise.resolve('data');
-}
-
-function wait(n) {
+function wait(n, msg) {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      res('hi');
+      res(msg || 'hi');
     }, n || 500);
   });
 }
 
 (async () => {
-  // console.log(await getJSON());
-  // console.log(await wait());
-  // console.log(await getJSON());
+  // Execute in sequence: each of these waits for resolution before the next one starts
+  console.log(await wait(100, '100'));
+  console.log(await wait(300, '300'));
 
-  // Execute in sequence:
-  console.log(await wait());
-  console.log(await wait());
-  console.log(await wait());
+  await wait(1000, '1000')
+    .catch(console.log) // some error-handling
+    .then(console.log); // post error-handling
 
   // Execute in parallel:
-  Promise.all([wait(), wait(), wait()]).then((data) => console.log(data));
+  let data = await Promise.all([wait(), wait(), wait()]);
+  console.log(data); // alternative to `.then(console.log);`
 })();
