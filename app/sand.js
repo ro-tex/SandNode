b67fdfcf1fc85dddb9879d0c80c96eed1414306b
Ivaylo Novakov <inovakov@gmail.com>
2018-05-04 15:54:19 +0200

    Play around with getters and setters. Use getters/setters to expose a local var which sits outside the class.

--- app/sand.js
+++ app/sand.js
@@ -1,6 +1,39 @@
 'use strict';
 
+let glob = 123;
+
 class Sand {
+  constructor(props) {
+    this.props = props;
+    this.field = 'default field value';
+  }
+
+  set field(newFieldVal) {
+    this._field = newFieldVal; // NOTE the actual var name can't be the same as the name of the setter/getter
+  }
+
+  get field() {
+    return this._field;
+  }
+
+  // a getter that exposes a global var (which is local to this class)
+  get [glob]() {
+    return glob;
+  }
+
+  set [glob](newGlob) {
+    glob = newGlob;
+  }
+
+  // a method using the getter
+  getGlob() {
+    if (!this[glob]) {
+      // if we don't have this the method will return `undefined` if we use the setter
+      this[glob] = glob;
+    }
+    return this[glob];
+  }
+
   static hello() {
     return 'Hello World!';
   }
