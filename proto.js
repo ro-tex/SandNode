cae8c2f7596a441842faf6f00f256ad845a69417
Ivaylo Novakov <inovakov@gmail.com>
2018-07-01 15:15:49 +0200

    Protocol buffers.

---
+++ proto.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// https://webapplog.com/json-is-not-cool-anymore/
+// https://medium.com/francesco-pongetti/using-protocol-buffers-in-a-node-js-angular-web-application-fba17df8ab51
+// https://github.com/google/protobuf
+
+// TODO https://github.com/dcodeIO/ProtoBuf.js#usage
+const protobuf = require('protobufjs');
