1794a416f5ce9c574f6c5187f1228948bcd69a9c
Ivaylo Novakov <inovakov@gmail.com>
2019-03-18 01:52:23 +0100

    protobufs

--- protocol_buffers/proto.js
+++ protocol_buffers/proto.js
@@ -2,5 +2,7 @@
 // https://medium.com/francesco-pongetti/using-protocol-buffers-in-a-node-js-angular-web-application-fba17df8ab51
 // https://github.com/google/protobuf
 
+// https://github.com/mafintosh/protocol-buffers
+
 // TODO https://github.com/dcodeIO/ProtoBuf.js#usage
 const protobuf = require('protobufjs');
