0408c47ee73d716ad48691b7af85755308bedcdf
Ivaylo Novakov <inovakov@gmail.com>
2019-05-11 22:48:47 +0200

    more TS

--- sand.ts
+++ sand.ts
@@ -1,34 +1,26 @@
-// function greeter(person) {
-//     return "Hello, " + person;
-// }
-//
-// let user = "Jane User";
-//
-// document.body.innerHTML = greeter(user);
-
-
+interface Person {
+    firstName: string;
+    lastName: string;
+}
 
 class Student {
     fullName: string; // default is public
+
+    // constructor params can auto-register fields
     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
 
-    static greet():void { // this is so static that you can't call it on an object!
+    static greet(): void { // this is so static that you can't call it on an object!
       console.log(`Hello World`);
     }
-}
-
-interface Person {
-    firstName: string;
-    lastName: string;
 }
 
 function greeter(person : Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
+Student.greet();
+
 let user = new Student("Jane", "M.", "User");
 console.log(user.fullName);
-
-// document.body.innerHTML = greeter(user);
